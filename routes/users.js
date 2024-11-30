be3f9d81eaa7ad5bab3e78f2d6b5c8cc3c9d3ce5
Brennan Tan Jien Nen <brennantan618@gmail.com>
2024-11-30 17:08:39 +0800

    Should return user id on successful login
    Should return user id on successful login to be used in session

--- routes/users.js
+++ routes/users.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
-    res.status(200).json({ message: 'Login successful!' });
+    res.status(200).json({ message: 'Login successful!', userId: user._id });
   } catch (error) {
     res.status(500).json({ 
       message: 'Error during login',
